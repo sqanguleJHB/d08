bed7b5dded9bb6afe17937a28037089aef031a1a
Sikho Qangule <sqangule@c4r10s10.wethinkcode.co.za>
2020-06-26 17:32:28 +0200

    first commit d8 ex00

---
+++ ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sqangule <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/26 17:30:10 by sqangule          #+#    #+#             */
+/*   Updated: 2020/06/26 17:31:59 by sqangule         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+#define FT_H
+
+void 	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_swap(int *a, int *b);
+int		ft_strlen(char *str);
+int 	ft_strcmp(char *s1, char *s2);
+
+#endif
